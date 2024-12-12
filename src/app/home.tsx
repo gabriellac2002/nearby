b79e9c03610c8d544ff32533158aadc59d45d06b
gabriellac2002 <gabriellacsilva2002@gmail.com>
2024-12-12 20:49:58 -0300

    add bottom sheet component and implement places fetching in Home component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,25 +2,47 @@ import { Alert, Text, View } from "react-native";
 import { api } from "./services/api";
 import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
 
+type Places = PlaceProps & {};
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [category, setCategory] = useState("");
 
+  const [places, setPlaces] = useState<Places[]>([]);
+
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
+      console.log("data", data);
       setCategories(data);
-      setCategory(data[0].id);
     } catch (error) {
       console.log(error);
       Alert.alert("Categorias", "Erro ao buscar categorias");
     }
   }
 
+  async function fetchPlaces() {
+    try {
+      if (!category) return;
+
+      const { data } = await api.get("/markets/category/" + category);
+      setPlaces(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Locais", "Erro ao buscar locais");
+    }
+  }
+
   useEffect(() => {
     fetchCategories();
   }, []);
+
+  useEffect(() => {
+    fetchPlaces();
+  }, [category]);
+
+  console.log(categories);
 
   return (
     <View style={{ flex: 1 }}>
---
+++ src/components/places/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import BottomSheet
+
+type Props = {
+  data: [];
+};
+
+export function Places({ data }: Props) {
+  return (
+
+  );
+}
