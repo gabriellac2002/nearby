0ee5ed34a8261fe238c9fb2a6143c7592be8ff4e
gabriellac2002 <gabriellacsilva2002@gmail.com>
2024-12-14 18:14:57 -0300

    add location functionality and integrate map view in Home component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,21 +1,46 @@
-import { Alert, Text, View } from "react-native";
+import { Alert, SafeAreaView, Text, View } from "react-native";
 import { api } from "./services/api";
 import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import { Loading } from "@/components/loading";
 
 type Places = PlaceProps & {};
+
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [category, setCategory] = useState("");
 
   const [places, setPlaces] = useState<Places[]>([]);
 
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+
+  async function getCurrentLocation() {
+    try {
+      let { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) {
+        Alert.alert("Localização", "É necessário permitir a localização");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
       console.log("data", data);
       setCategories(data);
+      setCategory(data[0].id);
     } catch (error) {
       console.log(error);
       Alert.alert("Categorias", "Erro ao buscar categorias");
@@ -35,6 +60,10 @@ export default function Home() {
   }
 
   useEffect(() => {
+    getCurrentLocation();
+  }, [location]);
+
+  useEffect(() => {
     fetchCategories();
   }, []);
 
@@ -42,15 +71,42 @@ export default function Home() {
     fetchPlaces();
   }, [category]);
 
-  console.log(categories);
+  if (!location) {
+    return <Loading />;
+  }
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, position: "relative" }}>
+      <MapView
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+        initialRegion={{
+          latitude: location?.coords.latitude || -23.55052,
+          longitude: location?.coords.longitude || -46.633308,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+      </MapView>
       <Categories
         data={categories}
         onSelected={setCategory}
         selected={category}
       />
+      <Places data={places} />
     </View>
   );
 }
--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -1,12 +1,38 @@
-import { View } from "react-native";
-import BottomSheet
+import { Text, useWindowDimensions, View } from "react-native";
+import BottomSheet, {
+  BottomSheetFlatList,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { useRef } from "react";
+import { Place, PlaceProps } from "../place";
+import { s } from "./styles";
 
 type Props = {
-  data: [];
+  data: PlaceProps[];
 };
 
 export function Places({ data }: Props) {
-  return (
+  const dimensions = useWindowDimensions();
+  const bottomSheetRef = useRef<BottomSheet>(null);
 
+  const snapPoints = [278, dimensions.height - 128];
+  return (
+    <BottomSheet
+      ref={bottomSheetRef}
+      snapPoints={snapPoints}
+      handleIndicatorStyle={s.indicator}
+      backgroundStyle={s.container}
+      enableOverDrag={false}
+    >
+      <BottomSheetFlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={s.content}
+        ListHeaderComponent={() => (
+          <Text style={s.title}>Explore locais perto de você</Text>
+        )}
+      />
+    </BottomSheet>
   );
 }
